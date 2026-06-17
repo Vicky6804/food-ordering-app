cf1dc7ec39031fbdc0eb40a5c5ada88620014174
unknown <vignesh.2211061@srec.ac.in>
2026-06-17 12:31:38 +0530

    test

--- src/components/Body.js
+++ src/components/Body.js
@@ -16,7 +16,9 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch("/api/swiggy");
+    const data = await fetch(
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.01020&lng=76.97010&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
+    );
     const json = await data.json();
     const resData =
       json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
